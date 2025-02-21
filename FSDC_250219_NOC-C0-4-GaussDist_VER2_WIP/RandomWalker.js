379260300fe0e2d77b635837ff80289b6e97ed05
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-02-22 07:05:04 +0800

    Update RandomWalker.js

--- FSDC_250219_NOC-C0-4-GaussDist_VER2_WIP/RandomWalker.js
+++ FSDC_250219_NOC-C0-4-GaussDist_VER2_WIP/RandomWalker.js
@@ -10,17 +10,19 @@ class RandomWalker{
     this.prob = prob;
   }
   display(){
-    stroke(120);
-    fill(255);
+    stroke(this.strokeRGBA);
+    fill(this.fillRGBA);
     ellipse(this.loc[0], this.loc[1], this.dim_size[0], this.dim_size[1]);
     
   }
   movement(){
     let r = random(0,1);
+    // Random Function:
     if ((r < this.prob) && this.probSet){
-    this.loc[0]+=abs(this.stepsize[0]);
-      this.loc[1]+=abs(this.stepsize[1]);
+    this.loc[0]+=this.stepsize[0];
+      this.loc[1]+=this.stepsize[1];
     } else {
+      // Random Gaussian Distribution
     this.loc[0]+=this.stepsize[0]*randomGaussian(5,5);
     this.loc[1]+=this.stepsize[1]*randomGaussian(5,5);
     }
