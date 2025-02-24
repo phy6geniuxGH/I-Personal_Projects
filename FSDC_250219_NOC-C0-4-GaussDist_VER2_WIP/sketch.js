6225f28a21be7cb2f8224e23f52c5c80103c0e70
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-02-25 03:52:57 +0800

    Update sketch.js

--- FSDC_250219_NOC-C0-4-GaussDist_VER2_WIP/sketch.js
+++ FSDC_250219_NOC-C0-4-GaussDist_VER2_WIP/sketch.js
@@ -1,42 +1,30 @@
 //Randomicity based on Gaussian Distribution 
 
-let RandWalker = [];
-let amountWalker = 1000;
-let fixed = true;
-let probSet = true;
-
 /**************
 * Dashboard 
 ***************/
 
-var loc = [0,0];
-var step = [5,5];
-var fRGBA = [255,255,255,255];
-var sRGBA = [120,120,120,255];
-var sW = 5;
-var dimSize = [2,2];
-var prob = 1;
+let RandWalker = [];
+let amountWalker = 1000;
+let probSet = true;
+let flick = true;
+let prob = 1.0;
 
 /***************
 Main Program
 ****************/
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
   for(let i = 0; i <= amountWalker;i++){
-    if (fixed){
-      loc = [random(-width/2,width/2),random(-height/2,height/2)];
-      step = [random(-1,1),random(-1,1)];
-      sRGBA = [255,255,255,random(0,255)];
-      fRGBA = sRGBA;
-      sW = random(2,4);
-      let s = random(0,5);
-      dimSize = [s,s];
-      RandWalker[i] = new RandomWalker(loc, step, fRGBA, sRGBA, sW, dimSize,probSet,prob);
-    }
-    else{
-      RandWalker[i] = new RandomWalker(loc, step, fRGBA, sRGBA, sW, dimSize,probSet,prob);
-    }
+    loc = [random(-width/2,width/2),random(-height/2,height/2)];
+    step = [random(-1,1),random(-1,1)];
+    sRGBA = [255,255,255,random(0,255)];
+    fRGBA = sRGBA;
+    sW = random(2,4);
+    let s = random(0,5);
+    dimSize = [s,s];
+    RandWalker[i] = new RandomWalker(loc, step, fRGBA, sRGBA, sW, dimSize,probSet,prob, flick);
   }
 }
 
