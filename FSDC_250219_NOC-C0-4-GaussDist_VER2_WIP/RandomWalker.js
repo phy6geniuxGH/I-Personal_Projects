0c7b9fa54fc77d56970ab467cd49d312cb037be8
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-02-26 02:27:13 +0800

    Update RandomWalker.js

--- FSDC_250219_NOC-C0-4-GaussDist_VER2_WIP/RandomWalker.js
+++ FSDC_250219_NOC-C0-4-GaussDist_VER2_WIP/RandomWalker.js
@@ -9,6 +9,8 @@ class RandomWalker{
     this.probSet = probSet;
     this.prob = prob;
     this.flickering = flick;
+    this.boundfactor_x = 1.0
+    this.boundfactor_y = 1.0
   }
   display(){
     if (this.flickering){
@@ -25,8 +27,14 @@ class RandomWalker{
     let r = random(0,1);
     // Random Function:
     if ((r < this.prob) && this.probSet){
-    this.loc[0]+=this.stepsize[0];
-      this.loc[1]+=this.stepsize[1];
+      if(this.loc[0] >width/2 || this.loc[0]< -width/2){
+        this.boundfactor_x=this.boundfactor_x*-1;
+      }
+    if(this.loc[1] >height/2 || this.loc[1]< -height/2){
+        this.boundfactor_y=this.boundfactor_y*-1;
+      }
+      this.loc[0]+=this.stepsize[0]*this.boundfactor_x;
+      this.loc[1]+=this.stepsize[1]*this.boundfactor_y;
     } else {
       // Random Gaussian Distribution
     this.loc[0]+=this.stepsize[0]*randomGaussian(5,5);
