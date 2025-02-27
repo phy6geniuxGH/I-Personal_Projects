40a2422371011581e680033dab669d03fb00d7cb
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-02-28 06:27:06 +0800

    Update sketch.js

--- FSDC_250219_NOC-C0-4-GaussDist_VER2_WIP/sketch.js
+++ FSDC_250219_NOC-C0-4-GaussDist_VER2_WIP/sketch.js
@@ -9,13 +9,14 @@ let amountWalker = 1000;
 let probSet = true;
 let flick = true;
 let prob = 1.0;
+let still_background = false;
 
 /***************
 Main Program
 ****************/
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(500, 500);
   for(let i = 0; i <= amountWalker;i++){
     loc = [random(-width/2,width/2),random(-height/2,height/2)];
     step = [random(-1,1),random(-1,1)];
@@ -26,10 +27,15 @@ function setup() {
     dimSize = [s,s];
     RandWalker[i] = new RandomWalker(loc, step, fRGBA, sRGBA, sW, dimSize,probSet,prob, flick);
   }
+  if(still_background){
+    background(0);
+  }
 }
 
 function draw() {
-  background(0);
+  if(still_background != true){
+    background(0);
+  }
   translate(width/2, height/2);
   for(let i = 0;i <= amountWalker;i++){
      RandWalker[i].display();
